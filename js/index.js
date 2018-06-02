182d3773ba84e77696952f4c81f68171701e0aed
Dhruv <dhruvdev7794@gmail.com>
2018-06-02 16:10:24 -0400

    added some changes and fixed some visual issues

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-function openTab(tabName){
-  var tabContent = document.getElementsByClassName("mainContent")
-  for(i=0; i<tabContent.length; i++){
-    tabContent[i].style.display = "none";
-  }
-  document.getElementById(tabName).style.display = "block"
-}
+// function openTab(tabName){
+//   var tabContent = document.getElementsByClassName("mainContent")
+//   for(i=0; i<tabContent.length; i++){
+//     tabContent[i].style.display = "none";
+//   }
+//   document.getElementById(tabName).style.display = "block"
+// }
 
-openTab('mainHead');
+// openTab('mainHead');
