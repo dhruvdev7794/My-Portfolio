266ac53d01d1e909e2a67541e145134a325d4ab4
Dhruv <dhruvdev7794@gmail.com>
2018-06-10 18:09:26 -0400

    removed useless files

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// function openTab(tabName){
-//   var tabContent = document.getElementsByClassName("mainContent")
-//   for(i=0; i<tabContent.length; i++){
-//     tabContent[i].style.display = "none";
-//   }
-//   document.getElementById(tabName).style.display = "block"
-// }
-
-// openTab('mainHead');
